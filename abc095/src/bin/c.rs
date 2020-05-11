a5bbc8b58718484a885c0499870620dad588da45
moisutsu <moisutsu@gmail.com>
2020-05-11 23:47:37 +0900

    add some solutions

--- abc095/src/bin/c.rs
+++ abc095/src/bin/c.rs
@@ -3,7 +3,26 @@ use proconio::*;
 #[fastout]
 fn main() {
     input! {
-        x: i32,
+        a: i32, b: i32, c: i32, mut x: i32, mut y: i32
     }
-    println!("{}", x);
+    let mut total = 0;
+    let lower = if a + b >= c * 2 { c * 2 } else { a + b };
+    let min = if x >= y { y } else { x };
+    total += lower * min;
+    if x - min == 0 {
+        let least = y - min;
+        if b <= c * 2 {
+            total += least * b;
+        } else {
+            total += least * c * 2;
+        }
+    } else {
+        let least = x - min;
+        if a <= c * 2 {
+            total += least * a;
+        } else {
+            total += least * c * 2;
+        }
+    }
+    println!("{}", total);
 }
