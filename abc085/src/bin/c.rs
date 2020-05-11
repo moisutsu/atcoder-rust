3b53dcc97259b8e3bfcc22b76cca43340c76cde1
moisutsu <moisutsu@gmail.com>
2020-05-11 17:20:16 +0900

    AtCoder Beginners Selection C

--- abc085/src/bin/c.rs
+++ abc085/src/bin/c.rs
@@ -2,7 +2,20 @@ use proconio::*;
 
 fn main() {
     input! {
-        x: i32,
+        n: usize, y: usize,
     }
-    println!("{}", x);
+    let mut ans_flag = false;
+    'outer: for i in 0..n + 1 {
+        for j in 0..n - i + 1 {
+            let k = n - i - j;
+            if y == i * 10000 + j * 5000 + k * 1000 {
+                print!("{} {} {}", i, j, k);
+                ans_flag = true;
+                break 'outer;
+            }
+        }
+    }
+    if !ans_flag {
+        print!("-1 -1 -1");
+    }
 }
--- abc086/src/bin/c.rs
+++ abc086/src/bin/c.rs
@@ -2,7 +2,24 @@ use proconio::*;
 
 fn main() {
     input! {
-        x: i32,
+        n: usize,
+        mut txys: [(i32, i32, i32); n],
     }
-    println!("{}", x);
+    txys.insert(0, (0, 0, 0));
+    let mut can_go = true;
+    for i in 1..n + 1 {
+        let dt: i32 = txys[i].0 - txys[i - 1].0;
+        let dist = (txys[i].1 - txys[i - 1].1).abs() + (txys[i].2 - txys[i - 1].2).abs();
+        if dist > dt {
+            can_go = false;
+            break;
+        }
+        if (dt % 2) != (dist % 2) {
+            can_go = false;
+            break;
+        }
+    }
+    println!("{}",
+        if can_go { "Yes" } else { "No" }
+    );
 }
