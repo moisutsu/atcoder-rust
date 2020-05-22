6082a2bf24ee1c0df4d4aab83de77a7d8bb6bbe5
moisutsu <moisutsu@gmail.com>
2020-05-22 12:21:39 +0900

    add some solutions

--- abc168/src/bin/d.rs
+++ abc168/src/bin/d.rs
@@ -1,12 +1,34 @@
 use proconio::*;
+use proconio::marker::*;
+
+use std::collections::VecDeque;
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
         N: usize, M: usize,
-        mut AB: [(usize, usize); M],
+        mut AB: [(Usize1, Usize1); M],
     }
-    let mut count = 0;
-    let mut miti: Vec<usize> = vec![0; N];
+    let mut edges = vec![vec![]; N];
+    for &(A, B) in AB.iter() {
+        edges[A].push(B);
+        edges[B].push(A);
+    }
+    let mut q = VecDeque::new();
+    q.push_front(0);
+    let mut numbers = vec![std::usize::MAX; N];
+    while q.len() != 0 {
+        let room = q.pop_back().unwrap();
+        for &each in edges[room].iter() {
+            if numbers[each] == std::usize::MAX {
+                numbers[each] = room;
+                q.push_front(each);
+            }
+        }
+    }
+    println!("Yes");
+    for &number in numbers[1..].iter() {
+        println!("{}", number + 1);
+    }
 }
