2c80229dc651d238ccf70a20317d8e3957dc33de
moisutsu <moisutsu@gmail.com>
2020-05-27 00:07:48 +0900

    change answer

--- abc139/src/bin/d.rs
+++ abc139/src/bin/d.rs
@@ -8,6 +8,10 @@ fn main() {
     input! {
         N: i128,
     }
-    let ans: i128 = (N * (N - 1i128)) / 2i128;
+    // let ans: i128 = (N * (N - 1i128)) / 2i128;
+    let mut ans: i128 = 0;
+    for i in 0..N {
+        ans += i;
+    }
     println!("{}", ans);
 }
