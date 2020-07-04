7b1a8a3305889d15a2e2885e08e2c9404f776705
moisutsu <moisutsu@gmail.com>
2020-07-04 21:32:55 +0900

    [Add] abc171 c

--- abc171/src/bin/c.rs
+++ abc171/src/bin/c.rs
@@ -6,7 +6,19 @@ use proconio::*;
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        mut N: usize,
+        mut N: Usize1,
     }
-    println!("{}", N);
+    let mut ids = vec![];
+    while N > 25 {
+        ids.push(N % 26);
+        N /= 26;
+        N -= 1;
+    }
+    ids.push(N);
+    let alphabets = "abcdefghijklmnopqrstuvwxyz".chars().collect::<Vec<char>>();
+    let mut ans = "".to_string();
+    for id in ids {
+        ans = format!("{}{}", alphabets[id], ans);
+    }
+    println!("{}", ans);
 }
---
+++ abc173/src/bin/d.rs
@@ -0,0 +1,13 @@
+use proconio::*;
+#[allow(unused_imports)]
+use proconio::marker::*;
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        x: i32,
+    }
+    let mut ans = 0;
+    println!("{}", ans);
+}
