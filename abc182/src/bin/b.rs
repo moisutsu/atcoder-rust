79b88ff4d97f5bf7cd99979be64eddc9b1e627ec
moisutsu <moisutsu@gmail.com>
2021-01-03 23:55:20 +0900

    Update abc182

--- abc182/src/bin/b.rs
+++ abc182/src/bin/b.rs
@@ -13,13 +13,27 @@ macro_rules! echo {
     }
 }
 
+
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        N: i32,
+        N: usize,
+        A: [usize; N],
     }
+    let mut gcd = 0;
     let mut ans = 0;
-    ans = N;
+    for k in 2..=1000 {
+        let mut tmp_gcd = 0;
+        for &a in A.iter() {
+            if a % k == 0 {
+                tmp_gcd += 1;
+            }
+        }
+        if tmp_gcd >= gcd {
+            gcd = tmp_gcd;
+            ans = k;
+        }
+    }
     echo!(ans);
 }
--- abc182/src/bin/c.rs
+++ abc182/src/bin/c.rs
@@ -1,6 +1,6 @@
-use proconio::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::*;
 
 #[allow(unused_macros)]
 macro_rules! echo {
@@ -17,9 +17,30 @@ macro_rules! echo {
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        N: i32,
+        N: Chars,
     }
-    let mut ans = 0;
-    ans = N;
+    let mut ans = -1;
+    'outer: for k in 0..N.len() {
+        for bit in 0..1 << N.len() {
+            let mut v = Vec::new();
+            for i in 0..N.len() {
+                if bit & (1 << i) != 0 {
+                    v.push(i);
+                }
+            }
+            if N.len() - v.len() != k {
+                continue;
+            }
+            let mut total = 0;
+            for &i in v.iter() {
+                total += N[i] as i32 - '0' as i32;
+            }
+            if total % 3 == 0 {
+                ans = k as i32;
+                break 'outer;
+            }
+        }
+    }
+
     echo!(ans);
 }
