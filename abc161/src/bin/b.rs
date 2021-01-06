807fd6f2c33733e423dc8ea0a84443e5d8b86c8b
moisutsu <moisutsu@gmail.com>
2021-01-06 17:02:55 +0900

    Update abc161

--- abc161/src/bin/b.rs
+++ abc161/src/bin/b.rs
@@ -1,18 +1,35 @@
-use std::io::{self, Read as _};
+use proconio::*;
+#[allow(unused_imports)]
+use proconio::marker::*;
 
+#[allow(unused_macros)]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
+
+macro_rules! YesNo {
+    ($ flag : expr ) => {
+        if $flag {
+            "Yes"
+        } else {
+            "No"
+        }
+    };
+}
+#[fastout]
+#[allow(non_snake_case)]
 fn main() {
-    let mut input = "".to_owned();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let mut input = input.split_whitespace();
-    macro_rules! read(
-        ([$tt:tt]) => (read!([$tt; read!(usize)]));
-        ([$tt:tt; $n:expr]) => ((0..$n).map(|_| read!($tt)).collect::<Vec<_>>());
-        (($($tt:tt),+)) => (($(read!($tt)),*));
-        (Usize1) => (read!(usize) - 1);
-        (Byte) => (read!(char) as u8);
-        (Bytes) => (read!(String).into_bytes());
-        ($ty:ty) => (input.next().unwrap().parse::<$ty>().unwrap());
-    );
-
-    let n = read!(usize);
+    input! {
+        N: usize, M: usize,
+        As: [usize; N],
+    }
+    let total = As.iter().sum::<usize>();
+    let ans = As.iter().filter(|&&A| A * 4 * M >= total).count() >= M;
+    echo!(YesNo!(ans));
 }
--- abc161/src/bin/c.rs
+++ abc161/src/bin/c.rs
@@ -1,18 +1,25 @@
-use std::io::{self, Read as _};
+use proconio::*;
+#[allow(unused_imports)]
+use proconio::marker::*;
 
+#[allow(unused_macros)]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
+
+#[fastout]
+#[allow(non_snake_case)]
 fn main() {
-    let mut input = "".to_owned();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let mut input = input.split_whitespace();
-    macro_rules! read(
-        ([$tt:tt]) => (read!([$tt; read!(usize)]));
-        ([$tt:tt; $n:expr]) => ((0..$n).map(|_| read!($tt)).collect::<Vec<_>>());
-        (($($tt:tt),+)) => (($(read!($tt)),*));
-        (Usize1) => (read!(usize) - 1);
-        (Byte) => (read!(char) as u8);
-        (Bytes) => (read!(String).into_bytes());
-        ($ty:ty) => (input.next().unwrap().parse::<$ty>().unwrap());
-    );
-
-    let n = read!(usize);
+    input! {
+        N: i32,
+    }
+    let mut ans = 0;
+    ans = N;
+    echo!(ans);
 }
