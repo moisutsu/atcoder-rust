322eb7e4f450e0ea20ab117eb98719ce141d959f
moisutsu <moisutsu@gmail.com>
2021-02-04 16:12:24 +0900

    Add dp C

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -1,6 +1,6 @@
-use proconio::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::*;
 
 #[allow(unused_macros)]
 macro_rules! echo {
@@ -13,13 +13,21 @@ macro_rules! echo {
     }
 }
 
+macro_rules ! max {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: max ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: max ($ a , max ! ($ ($ rest ) ,+ ) ) } } ; }
+macro_rules ! chmax {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_max = max ! ($ ($ cmps ) ,+ ) ; if $ base < cmp_max {$ base = cmp_max ; true } else {false } } } ; }
+
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        N: i32,
+        N: usize,
+        abc: [(i128, i128, i128); N],
     }
-    let mut ans = 0;
-    ans = N;
-    echo!(ans);
+    let mut dp = vec![(0, 0, 0); N + 1];
+    for i in 0..N {
+        chmax!(dp[i + 1].0, dp[i].1 + abc[i].1, dp[i].2 + abc[i].2);
+        chmax!(dp[i + 1].1, dp[i].0 + abc[i].0, dp[i].2 + abc[i].2);
+        chmax!(dp[i + 1].2, dp[i].1 + abc[i].1, dp[i].0 + abc[i].0);
+    }
+    echo!(max!(dp[N].0, dp[N].1, dp[N].2));
 }
