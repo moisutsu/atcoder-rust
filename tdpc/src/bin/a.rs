bc3ce93db09d3f862661aa951a0253c9fd3bf2ed
moisutsu <moisutsu@gmail.com>
2021-02-04 17:37:36 +0900

    Add tdpc A

--- tdpc/src/bin/a.rs
+++ tdpc/src/bin/a.rs
@@ -1,6 +1,6 @@
-use proconio::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::*;
 
 #[allow(unused_macros)]
 macro_rules! echo {
@@ -17,9 +17,20 @@ macro_rules! echo {
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        N: i32,
+        N: usize,
+        ps: [usize; N],
     };
-    let mut ans = 0;
-    ans = N;
-    echo!(ans);
+    let mut dp = vec![vec![false; 10000]; N + 1];
+    for n_i in 0..=N {
+        dp[n_i][0] = true;
+    }
+    for n_i in 0..N {
+        for p_i in 0..10000 {
+            if dp[n_i][p_i] && p_i + ps[n_i] < 10000 {
+                dp[n_i + 1][p_i] = true;
+                dp[n_i + 1][p_i + ps[n_i]] = true;
+            }
+        }
+    }
+    echo!(dp[N].iter().filter(|&&x| x).count());
 }
--- tdpc/src/bin/b.rs
+++ tdpc/src/bin/b.rs
@@ -1,6 +1,6 @@
+use proconio::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
-use proconio::*;
 
 #[allow(unused_macros)]
 macro_rules! echo {
@@ -13,24 +13,13 @@ macro_rules! echo {
     }
 }
 
-macro_rules ! min {($ a : expr $ (, ) * ) => {{$ a } } ; ($ a : expr , $ b : expr $ (, ) * ) => {{std :: cmp :: min ($ a , $ b ) } } ; ($ a : expr , $ ($ rest : expr ) ,+ $ (, ) * ) => {{std :: cmp :: min ($ a , min ! ($ ($ rest ) ,+ ) ) } } ; }
-macro_rules ! chmin {($ base : expr , $ ($ cmps : expr ) ,+ $ (, ) * ) => {{let cmp_min = min ! ($ ($ cmps ) ,+ ) ; if $ base > cmp_min {$ base = cmp_min ; true } else {false } } } ; }
-
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        N: usize, K: usize,
-        hs: [i128; N],
+        N: i32,
     };
-    let mut dp = vec![std::i128::MAX / 2; N];
-    dp[0] = 0;
-    for n_i in 0..N {
-        for k_i in 1..=K {
-            if n_i >= k_i {
-                chmin!(dp[n_i], dp[n_i - k_i] + (hs[n_i] - hs[n_i - k_i]).abs());
-            }
-        }
-    }
-    echo!(dp[N]);
+    let mut ans = 0;
+    ans = N;
+    echo!(ans);
 }
