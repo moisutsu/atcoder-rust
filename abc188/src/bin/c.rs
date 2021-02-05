2f2bf24d1da17b42cb8413ccb3874323d26416bb
moisutsu <moisutsu@gmail.com>
2021-02-05 23:45:15 +0900

    Add abc188 C

--- abc188/src/bin/c.rs
+++ abc188/src/bin/c.rs
@@ -1,6 +1,7 @@
-use proconio::*;
 #[allow(unused_imports)]
 use proconio::marker::*;
+use proconio::*;
+use std::cmp::{max, min};
 
 #[allow(unused_macros)]
 macro_rules! echo {
@@ -19,8 +20,16 @@ fn main() {
     input! {
         N: u32,
         As: [i128; 2usize.pow(N)]
+    };
+    let mut players = As.clone();
+    for p_i in (1..N as usize).rev() {
+        let new_length = 2usize.pow(p_i as u32);
+        let mut new_players = vec![0; new_length];
+        for w_i in 0..new_length {
+            new_players[w_i] = max(players[2 * w_i], players[2 * w_i + 1]);
+        }
+        players = new_players
     }
-    let mut ans = 0;
-    ans = N;
-    echo!(ans);
+    let ans_rate = min(players[0], players[1]);
+    echo!(As.iter().position(|&x| x == ans_rate).unwrap() + 1);
 }
