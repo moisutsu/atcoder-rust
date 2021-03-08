931db3702fdbb3029131ec14911b58ab8650d06e
moisutsu <moisutsu@gmail.com>
2021-03-08 14:45:57 +0900

    Update abc023 D

--- abc023/src/bin/d.rs
+++ abc023/src/bin/d.rs
@@ -1,8 +1,68 @@
 #[allow(unused_imports)]
-use proconio::marker::*;
-use proconio::*;
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::fastout;
+#[allow(unused_imports)]
+use std::{
+    cell::RefCell,
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+    fmt::Debug,
+    io::{self, BufRead, Read},
+    rc::Rc,
+    str::{FromStr, SplitWhitespace},
+};
+
+trait Sorted {
+    type Element;
+    fn sorted(&self) -> Vec<Self::Element>;
+    fn sorted_by_key<F: FnMut(&Self::Element) -> K, K: Ord>(&self, f: F) -> Vec<Self::Element>;
+}
+impl<T> Sorted for [T]
+where
+    T: Clone + Ord,
+{
+    type Element = T;
+    fn sorted(&self) -> Vec<Self::Element> {
+        let mut v = self.clone().to_vec();
+        v.sort();
+        v
+    }
+    fn sorted_by_key<F: FnMut(&Self::Element) -> K, K: Ord>(&self, f: F) -> Vec<Self::Element> {
+        let mut v = self.clone().to_vec();
+        v.sort_by_key(f);
+        v
+    }
+}
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        hs: [(i128, i128); N]
+    };
+    const INF: i128 = 1 << 60;
+    let mut left = 0;
+    let mut right = INF;
+    'outer: while right - left > 1 {
+        let mid = (left + right) / 2;
+        let hs = hs.sorted_by_key(|&(h, s)| (mid - h) / s);
+        let mut t = 0;
+        for (h, s) in hs {
+            if h + s * t > mid {
+                left = mid;
+                continue 'outer;
+            }
+            t += 1;
+        }
+        right = mid;
+    }
+    echo!(right);
+}
 
 #[allow(unused_macros)]
+#[macro_export]
 macro_rules! echo {
     ($($e:expr),*) => {
         let mut s = Vec::new();
@@ -13,41 +73,187 @@ macro_rules! echo {
     }
 }
 
-#[fastout]
-#[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: usize,
-        HS: [(i128, i128); N],
+#[macro_export]
+macro_rules! input {
+    (from $scanner:ident; $($tt:tt)*) => {
+        $crate::input_inner!(@scanner($scanner), @tts($($tt)*))
     };
-
-    const INF: i128 = 1 << 60;
-    let (mut left, mut right) = (0, INF);
-    while right - left > 1 {
-        let mut ok = true;
-        let mid = (left + right) / 2;
-
-        let mut remain_times = vec![0; N];
-        for i in 0..N {
-            if HS[i].0 > mid {
-                ok = false;
-            }
-            remain_times[i] = (mid - HS[i].0) / HS[i].1;
+    ($($tt:tt)*) => {
+        let __scanner = $crate::DEFAULT_SCANNER.with(|__scanner| __scanner.clone());
+        let mut __scanner_ref = __scanner.borrow_mut();
+        if let $crate::Scanner::Uninited = *__scanner_ref {
+            *__scanner_ref = $crate::Scanner::stdin_auto().unwrap();
         }
-        remain_times.sort_unstable();
-        for i in 0..N {
-            if remain_times[i] < i as i128 {
-                ok = false;
-                break;
+        $crate::input_inner!(@scanner(__scanner_ref), @tts($($tt)*));
+        ::std::mem::drop(__scanner_ref);
+        ::std::mem::drop(__scanner);
+    };
+}
+
+#[macro_export]
+macro_rules! input_inner {
+    (@scanner($scanner:ident), @tts()) => {};
+    (@scanner($scanner:ident), @tts(mut $single_tt_pat:tt : $readable:tt)) => {
+        let mut $single_tt_pat = $crate::read!(from $scanner { $readable });
+    };
+    (@scanner($scanner:ident), @tts($single_tt_pat:tt : $readable:tt)) => {
+        let $single_tt_pat = $crate::read!(from $scanner { $readable });
+    };
+    (@scanner($scanner:ident), @tts(mut $single_tt_pat:tt : $readable:tt, $($rest:tt)*)) => {
+        $crate::input_inner!(@scanner($scanner), @tts(mut $single_tt_pat: $readable));
+        $crate::input_inner!(@scanner($scanner), @tts($($rest)*));
+    };
+    (@scanner($scanner:ident), @tts($single_tt_pat:tt : $readable:tt, $($rest:tt)*)) => {
+        $crate::input_inner!(@scanner($scanner), @tts($single_tt_pat: $readable));
+        $crate::input_inner!(@scanner($scanner), @tts($($rest)*));
+    };
+}
+
+#[macro_export]
+macro_rules! read {
+    (from $scanner:ident { [$tt:tt] }) => {
+        $crate::read!(from $scanner { [$tt; $crate::read!(from $scanner { usize })] })
+    };
+    (from $scanner:ident  { [$tt:tt; $n:expr] }) => {
+        (0..$n).map(|_| $crate::read!(from $scanner { $tt })).collect::<Vec<_>>()
+    };
+    (from $scanner:ident { ($($tt:tt),+) }) => {
+        ($($crate::read!(from $scanner { $tt })),*)
+    };
+    (from $scanner:ident { { $f:expr } }) => {
+        $crate::FnOnceExt::<_>::call_once_from_reader($f, &mut $scanner)
+    };
+    (from $scanner:ident { $ty:ty }) => {
+        <$ty as $crate::Readable>::read_from_scanner(&mut $scanner)
+    };
+}
+
+#[macro_export]
+macro_rules! readable {
+    ($name:ident; |$scanner:ident| { $($body:tt)* }) => {
+        $crate::readable!($name; |$scanner| -> () { $($body)* });
+    };
+    ($name:ident; |$scanner:ident| $expr:expr) => {
+        $crate::readable!($name; |$scanner| -> () { $expr });
+    };
+    ($name:ident; |$scanner:ident| -> $output:ty { $($body:tt)* }) => {
+        enum $name {}
+
+        impl $crate::Readable for $name {
+            type Output = $output;
+
+            fn read_from_scanner(mut $scanner: &mut $crate::Scanner) -> $output {
+                $($body)*
             }
         }
+    };
+}
 
-        if ok {
-            right = mid;
+#[inline]
+pub fn usize1(n: usize) -> usize {
+    n - 1
+}
+
+#[inline]
+pub fn bytes(s: String) -> Vec<u8> {
+    s.into()
+}
+
+#[inline]
+pub fn chars(s: String) -> Vec<char> {
+    s.chars().collect()
+}
+
+#[doc(hidden)]
+trait FnOnceExt<A> {
+    type Output;
+    fn call_once_from_reader(this: Self, scanner: &mut Scanner) -> Self::Output;
+}
+
+impl<A, O, F> FnOnceExt<A> for F
+where
+    A: FromStr,
+    A::Err: Debug,
+    F: FnOnce(A) -> O,
+{
+    type Output = O;
+
+    #[inline]
+    fn call_once_from_reader(this: Self, scanner: &mut Scanner) -> O {
+        this(A::read_from_scanner(scanner))
+    }
+}
+pub enum Scanner {
+    Uninited,
+    Once {
+        words: SplitWhitespace<'static>,
+    },
+    Lines {
+        rdr: Box<dyn BufRead>,
+        words: SplitWhitespace<'static>,
+    },
+}
+
+impl Scanner {
+    fn stdin_auto() -> io::Result<Self> {
+        if cfg!(debug_assertions) {
+            Ok(Self::lines(Box::leak(Box::new(io::stdin())).lock()))
         } else {
-            left = mid;
+            Self::once(io::stdin())
         }
     }
 
-    echo!(right);
+    fn once<R: Read>(mut rdr: R) -> io::Result<Self> {
+        let mut buf = String::with_capacity(1024);
+        rdr.read_to_string(&mut buf)?;
+        let words = Box::leak(buf.into_boxed_str()).split_whitespace();
+        Ok(Self::Once { words })
+    }
+
+    fn lines<R: BufRead + 'static>(rdr: R) -> Self {
+        Self::Lines {
+            rdr: Box::new(rdr),
+            words: "".split_whitespace(),
+        }
+    }
+
+    fn parse_next_unwrap<T: FromStr>(&mut self) -> T
+    where
+        T::Err: Debug,
+    {
+        match self {
+            Self::Uninited => None,
+            Self::Once { words } => words.next(),
+            Self::Lines { rdr, words } => words.next().or_else(|| {
+                let mut line = "".to_owned();
+                rdr.read_line(&mut line).unwrap();
+                *words = Box::leak(line.into_boxed_str()).split_whitespace();
+                words.next()
+            }),
+        }
+        .expect("reached EOF")
+        .parse()
+        .unwrap()
+    }
+}
+
+thread_local! {
+    static DEFAULT_SCANNER: Rc<RefCell<Scanner>> = Rc::new(RefCell::new(Scanner::Uninited));
+}
+
+trait Readable {
+    type Output;
+
+    fn read_from_scanner(scanner: &mut Scanner) -> Self::Output;
+}
+
+impl<T: FromStr> Readable for T
+where
+    T::Err: Debug,
+{
+    type Output = Self;
+
+    fn read_from_scanner(scanner: &mut Scanner) -> Self {
+        scanner.parse_next_unwrap()
+    }
 }
