2d8c6d49d5550cdcfe216f33e9015a4663b43b02
moisutsu <moisutsu@gmail.com>
2021-03-24 01:26:00 +0900

    Add abc177 d

--- abc177/src/bin/d.rs
+++ abc177/src/bin/d.rs
@@ -1,8 +1,88 @@
 #[allow(unused_imports)]
-use proconio::marker::*;
-use proconio::*;
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(Usize1, Usize1); M],
+    };
+    let mut ans = 0;
+    let mut uf = UnionFind::new(N);
+    for (A, B) in AB {
+        uf.union(A, B);
+    }
+    let mut groups = HashSet::new();
+    for n_i in 0..N {
+        groups.insert(uf.find(n_i));
+    }
+    for group in groups.into_iter() {
+        ans = max(ans, uf.size(group));
+    }
+    echo!(ans);
+}
+
+pub struct UnionFind {
+    parents: Vec<usize>,
+    sizes: Vec<usize>,
+    groups_count: usize,
+}
+impl UnionFind {
+    pub fn new(n: usize) -> Self {
+        UnionFind {
+            parents: (0..n).collect(),
+            sizes: vec![1; n],
+            groups_count: n,
+        }
+    }
+    pub fn find(&mut self, x: usize) -> usize {
+        let parent_x = self.parents[x];
+        if parent_x == x {
+            x
+        } else {
+            let root_x = self.find(parent_x);
+            self.parents[x] = root_x;
+            root_x
+        }
+    }
+    pub fn union(&mut self, x: usize, y: usize) -> bool {
+        let (root_x, root_y) = (self.find(x), self.find(y));
+        if root_x == root_y {
+            return false;
+        }
+        if self.sizes[root_x] >= self.sizes[root_y] {
+            self.parents[root_y] = root_x;
+            self.sizes[root_x] += self.sizes[root_y];
+        } else {
+            self.parents[root_x] = root_y;
+            self.sizes[root_y] += self.sizes[root_x];
+        }
+        self.groups_count -= 1;
+        true
+    }
+    pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    pub fn groups_count(&self) -> usize {
+        self.groups_count
+    }
+    pub fn size(&mut self, x: usize) -> usize {
+        let root_x = self.find(x);
+        self.sizes[root_x]
+    }
+}
 
 #[allow(unused_macros)]
+#[macro_export]
 macro_rules! echo {
     ($($e:expr),*) => {
         let mut s = Vec::new();
@@ -11,15 +91,4 @@ macro_rules! echo {
         )*
         println!("{}" , s.join(" "));
     }
-}
-
-#[fastout]
-#[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: i32,
-    }
-    let mut ans = 0;
-    ans = N;
-    echo!(ans);
 }
