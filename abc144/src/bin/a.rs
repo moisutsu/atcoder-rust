17edf75148343d97bda8365544a30cc503229a0a
moisutsu <moisutsu@gmail.com>
2021-03-31 22:31:07 +0900

    Add abc144 A

--- abc144/src/bin/a.rs
+++ abc144/src/bin/a.rs
@@ -1,9 +1,34 @@
-use proconio::*;
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
 
 #[fastout]
+#[allow(non_snake_case)]
 fn main() {
     input! {
-        x: i32,
+        A: i64, B: i64,
+    };
+    echo!(if 1 <= A && A <= 9 && 1 <= B && B <= 9 {
+        A * B
+    } else {
+        -1
+    });
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
     }
-    println!("{}", x);
 }
