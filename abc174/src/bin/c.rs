12bdfe0e576491e18ead809d9eb78081fdd090b4
moisutsu <moisutsu@gmail.com>
2021-04-03 15:00:16 +0900

    Add abc174 C

--- abc174/src/bin/c.rs
+++ abc174/src/bin/c.rs
@@ -1,13 +1,45 @@
 #[allow(unused_imports)]
-use proconio::marker::*;
-use proconio::*;
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        K: usize
+        k: i64,
+    };
+    let mut set = HashSet::new();
+    let mut ans = -1;
+    let mut n = 7;
+    for i in 1..=k {
+        if n % k == 0 {
+            ans = i;
+            break;
+        }
+        if set.contains(&n) {
+            break;
+        }
+        set.insert(n);
+        n = (n * 10 + 7) % k
     }
-    let mut ans = 0;
-    println!("{}", ans);
+
+    echo!(ans);
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
 }
