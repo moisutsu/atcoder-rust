4a7326c548b8b9b7bce325a306ab09c12e11f735
moisutsu <moisutsu@gmail.com>
2021-04-06 23:51:05 +0900

    Add abc166 C

--- abc166/src/bin/c.rs
+++ abc166/src/bin/c.rs
@@ -1,10 +1,49 @@
-use proconio::*;
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        x: i32,
+        n: usize,
+        m: usize,
+        hs: [i64; n],
+        ab: [(Usize1, Usize1); m],
+    };
+    let mut g = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
     }
-    println!("{}", x);
+    let mut ans = 0;
+
+    'outer: for i in 0..n {
+        for &j in &g[i] {
+            if hs[i] <= hs[j] {
+                continue 'outer;
+            }
+        }
+        ans += 1;
+    }
+
+    echo!(ans);
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
 }
