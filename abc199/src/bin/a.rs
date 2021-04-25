31d7bddb751f7ff62e9b8e265254ae6784b412e9
moisutsu <moisutsu@gmail.com>
2021-04-25 23:04:59 +0900

    Add abc199

---
+++ abc199/src/bin/a.rs
@@ -0,0 +1,40 @@
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+macro_rules! YesNo {
+    ($ flag : expr ) => {
+        if $flag {
+            "Yes"
+        } else {
+            "No"
+        }
+    };
+}
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        a: i64, b: i64, c: i64,
+    };
+    echo!(YesNo!(a.pow(2) + b.pow(2) < c.pow(2)));
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
