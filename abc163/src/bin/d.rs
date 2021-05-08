a50b0b1bcfb364e0c0aabc56bd2b94a21c227e85
moisutsu <moisutsu@gmail.com>
2021-05-08 09:47:08 +0900

    Add abc163 D

--- abc163/src/bin/d.rs
+++ abc163/src/bin/d.rs
@@ -1,11 +1,39 @@
-use proconio::*;
-use proconio::marker::*;
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+const MOD: i64 = 1_000_000_007;
 
 #[fastout]
 #[allow(non_snake_case)]
 fn main() {
     input! {
-        x: i32,
+        n: i64, k: i64,
+    };
+    let mut ans = 0;
+    for k_i in k..=n + 1 {
+        let min_value = k_i * (k_i - 1) / 2;
+        let max_value = k_i * (n - k_i + 1 + n) / 2;
+        ans += max_value - min_value + 1;
+        ans %= MOD;
     }
-    println!("{}", x);
+    echo!(ans);
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
 }
