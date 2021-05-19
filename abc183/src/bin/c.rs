f05f8c355b99e7d7dda465493f6fbaa9ca34fdd8
moisutsu <moisutsu@gmail.com>
2021-05-19 11:46:36 +0900

    Add abc183 C

--- abc183/src/bin/c.rs
+++ abc183/src/bin/c.rs
@@ -1,8 +1,39 @@
 #[allow(unused_imports)]
-use proconio::marker::*;
-use proconio::*;
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        n: usize, k: i64,
+        t: [[i64; n]; n],
+    };
+    let mut ans = 0;
+
+    for mut vs in (1..n).permutations(n - 1) {
+        vs.insert(0, 0);
+        vs.push(0);
+        let mut time = 0;
+        for (from, to) in vs.into_iter().tuple_windows() {
+            time += t[from][to];
+        }
+        if time == k {
+            ans += 1;
+        }
+    }
+
+    echo!(ans);
+}
 
 #[allow(unused_macros)]
+#[macro_export]
 macro_rules! echo {
     ($($e:expr),*) => {
         let mut s = Vec::new();
@@ -11,15 +42,4 @@ macro_rules! echo {
         )*
         println!("{}" , s.join(" "));
     }
-}
-
-#[fastout]
-#[allow(non_snake_case)]
-fn main() {
-    input! {
-        N: i32,
-    }
-    let mut ans = 0;
-    ans = N;
-    echo!(ans);
 }
