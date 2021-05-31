c18d77409d833eb317dd7a9e16891f44edd32583
moisutsu <moisutsu@gmail.com>
2021-05-31 17:46:35 +0900

    Add abc203

---
+++ abc203/src/bin/b.rs
@@ -0,0 +1,36 @@
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        n: i64,
+        k: i64,
+    };
+    let mut ans = 0;
+    for (n_i, k_i) in iproduct!(1..=n, 1..=k) {
+        let room_number: i64 = format!("{}0{}", n_i, k_i).parse().unwrap();
+        ans += room_number;
+    }
+    echo!(ans);
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
