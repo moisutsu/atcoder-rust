b3c2f498d30a2e298f8233377d953f019a15dc67
moisutsu <moisutsu@gmail.com>
2021-07-31 22:42:24 +0900

    Add abc209

---
+++ abc209/src/bin/a.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        a: i64,
+        b: i64
+    };
+    echo!(max(0, b - a + 1));
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
---
+++ abc209/src/bin/b.rs
@@ -0,0 +1,47 @@
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+macro_rules! YesNo {
+    ($ flag : expr ) => {
+        if $flag {
+            "Yes"
+        } else {
+            "No"
+        }
+    };
+}
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        n: usize,
+        x: i64,
+        a: [i64; n]
+    };
+    let total = a
+        .into_iter()
+        .enumerate()
+        .map(|(i, p)| if i % 2 == 0 { p } else { p - 1 })
+        .sum::<i64>();
+    echo!(YesNo!(total <= x));
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
