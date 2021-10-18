3692a0b204f9c535b7f061592b4fdc71de8968c6
moisutsu <moisutsu@gmail.com>
2021-10-18 19:42:14 +0900

    Add abc210

---
+++ abc210/src/bin/a.rs
@@ -0,0 +1,33 @@
+#[allow(unused_imports)]
+use itertools::*;
+#[allow(unused_imports)]
+use proconio::{marker::*, *};
+#[allow(unused_imports)]
+use std::{
+    cmp::{max, min, Reverse},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
+};
+
+#[fastout]
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        n: i64,
+        a: i64,
+        x: i64,
+        y: i64
+    };
+    echo!(x * min(n, a) + y * max(0, n - a));
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! echo {
+    ($($e:expr),*) => {
+        let mut s = Vec::new();
+        $(
+            s.push(format!("{}" , $e));
+        )*
+        println!("{}" , s.join(" "));
+    }
+}
